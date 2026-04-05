d020477de1dee77da20f2a1c9306ccd2e3824de4
Fernando Regis <nando.1regis@gmail.com>
2026-04-05 14:55:31 -0300

    botão no header de produto

--- src/pages/product/Index.jsx
+++ src/pages/product/Index.jsx
@@ -1,17 +1,44 @@
+import { useNavigate } from "react-router-dom";
+import PlusIcon from "../../components/svg/PlusIcon";
 
 
 const ProductPage = ({children}) =>  {
-
+    const navigate = useNavigate();
 
     return(<>
         <div className="min-h-screen w-full bg-gray-50 font-sans">
-           {  
-            // header aqui
-           }
-            <div className="w-full bg-white border-b border-gray-100 px-6 py-4">
-                <h1 className="text-base font-medium text-gray-900">Produtos</h1>
-                <p className="text-sm text-gray-500 mt-0.5">Gerencie seus produtos</p>
+         
+            <div className="flex justify-between items-center w-full bg-white border-b border-gray-100 px-6 py-4">
+                <div>
+                    <h1 className="text-base font-medium text-gray-900">Produtos</h1>
+                    <p className="text-sm text-gray-500 mt-0.5">Gerencie seus produtos</p>
+                </div>
+                <div>
+                    <button 
+                        onClick={() => {
+                            navigate("/produtos/new");
+                        }}
+                        type="button" 
+                        className="
+                        cursor-pointer
+                        flex items-center gap-2 
+                        px-3 py-2 
+                        bg-gray-800 text-white 
+                        shadow-md 
+                        rounded-md
+                        hover:bg-gray-700 
+                        active:scale-95 
+                        transition-all duration-200 
+                        font-medium
+                    ">
+                        <PlusIcon/>
+                        <span>Criar Produto</span>
+                    </button>
+                </div>
+
             </div>
+
+            
 
              <div className="p-6">
                {children}
