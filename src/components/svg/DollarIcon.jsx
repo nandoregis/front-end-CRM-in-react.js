bde3e7b15aef61305579b16c5af279945c8e65df
Fernando Regis <nando.1regis@gmail.com>
2026-04-05 21:22:58 -0300

    novo icone do dolar

---
+++ src/components/svg/DollarIcon.jsx
@@ -0,0 +1,11 @@
+const DollarIcon = () => {
+  return (
+    <svg width={15} height={15} viewBox="0 0 24 24" fill="none" stroke="currentColor"
+      strokeWidth={1.8} strokeLinecap="round" strokeLinejoin="round">
+      <line x1="12" y1="1" x2="12" y2="23" />
+      <path d="M17 5H9.5a3.5 3.5 0 000 7h5a3.5 3.5 0 010 7H6" />
+    </svg>
+  );
+};
+
+export default DollarIcon;
