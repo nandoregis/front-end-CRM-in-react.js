632749f90c22ace5a61b7b888728f8872bb704e9
Fernando Regis <nando.1regis@gmail.com>
2026-04-06 21:03:06 -0300

    removendo código, inutil

--- src/pages/cores/components/ColorCreate/Index.jsx
+++ src/pages/cores/components/ColorCreate/Index.jsx
@@ -1,13 +1,7 @@
 import { useState } from "react";
 import Api from "../../../../services/Api";
 import { useToast } from "../../../../context/ToastContext";
-
-const BackIcon = () => (
-  <svg width={16} height={16} viewBox="0 0 24 24" fill="none" stroke="currentColor"
-    strokeWidth={1.8} strokeLinecap="round" strokeLinejoin="round">
-    <path d="M19 12H5M12 5l-7 7 7 7" />
-  </svg>
-);
+import BackIcon from "../../../../components/svg/BackIcon";
 
 const ColorCreate = ({ onVoltar }) => {
   const [form, setForm] = useState({ name: "", hex: "#000000" });
